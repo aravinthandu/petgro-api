a41995bad96edea5137ef8f7ee3b21e62e606db4
anuram.srinivasan <anuram.srinivasan@capestart.com>
2022-05-22 23:31:32 -0700

    Removed unwanted files

--- src/models/user.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface UserDetails {
-    id?:number;
-    name?: string;
-    email?: string;
-    phone?: string;
-    isActive?: boolean;
-    accountId?: string;
-    createdAt?:Date;
-}
